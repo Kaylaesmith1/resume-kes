412103c8b1b290bccbeda889e8e75df6f711f8a5
Kayla S <kaylaesmith1@gmail.com>
2022-12-28 15:24:15 +0000

    Obtain user info from GitHub

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,16 +2,17 @@ function userInformationHTML(user) {
     return `
     <h2>${user.name}
     <span class="small-name">
-    (@<a href="${user.html_url}" target="_blank"> ${user.login}</a>
+    (@<a href="${user.html_url}" target="_blank"> ${user.login}</a>)
     </span>
     </h2>
     <div class="gh-content">
-        <div clas="gh-avatar">
+        <div class="gh-avatar">
             <a href="${user.html_url} target="_blank">
                 <img src="${user.avatar_url}" width="80" height="80" alt"${user.login}"/>
              </a>
          </div>
-    </div>`
+         <p>Followers: ${user.followers}<br>Following: ${user.following} <br>Repositories: ${user.public_repos}</p>
+    </div>`;
 }
 
 function fetchGitHubInformation(event) {
