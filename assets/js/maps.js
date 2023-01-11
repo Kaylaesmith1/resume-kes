c222fd9a4cdadd8a760ad7619016d2837fce0f7a
Kayla S <kaylaesmith1@gmail.com>
2023-01-11 23:15:55 +0000

    Final aesthetics for the webpage. Change Google Maps API locations.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,17 +1,24 @@
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 3,
+        zoom: 1.75,
         center: {
             lat: 46.619261,
-            lng: 9.134766
+            lng: -39.134766
             // lng: -33.134766
         }
     });
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var locations = [
-        {lat: 48.8080, lng: 9.4259},
-        {lat: 39.6994, lng: 3.0063},
-        {lat: 54.3520, lng: 18.6466}
+        
+        // Mallorca //
+        // {lat: 39.6994, lng: 3.0063},
+        // Gdańsk //
+        // {lat: 54.3520, lng: 18.6466},
+        // BCN //
+        {lat: 41.390205, lng: 2.154007},
+        // DSM //
+        {lat: 41.5878, lng: -93.6192}
+        
     ];
     var marker = locations.map(function(location, i) {
         return new google.maps.Marker({
