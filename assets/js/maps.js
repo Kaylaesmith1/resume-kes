d5109de5a219a3f4a932c9f48b1e7f67b6a4c975
Kayla S <kaylaesmith1@gmail.com>
2023-01-12 03:15:15 +0000

    Final aesthetics of website. Finish interests section.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -4,16 +4,10 @@ function initMap() {
         center: {
             lat: 46.619261,
             lng: -39.134766
-            // lng: -33.134766
         }
     });
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var locations = [
-        
-        // Mallorca //
-        // {lat: 39.6994, lng: 3.0063},
-        // Gdańsk //
-        // {lat: 54.3520, lng: 18.6466},
         // BCN //
         {lat: 41.390205, lng: 2.154007},
         // DSM //
