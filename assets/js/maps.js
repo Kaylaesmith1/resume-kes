ef146f1e8cd8f910d3acbd79382ed959cc7c4bf4
Kayla S <kaylaesmith1@gmail.com>
2025-03-25 16:02:53 +0000

    change centre of map

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,14 +1,15 @@
 function initMap() {
     // Set Barcelona as the center of the map
     const barcelona = { lat: 41.390205, lng: 2.154007 };
+    const teruel = { lat: 40.3457, lng: 1.1064};
 
-    // Create the map
+    
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,  // Adjust zoom level as needed
-        center: barcelona,
+        zoom: 4, 
+        center: teruel,
     });
 
-    // Add a marker in Barcelona
+    
     new google.maps.Marker({
         position: barcelona,
         map: map,
