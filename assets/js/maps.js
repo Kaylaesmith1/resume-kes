9c7dcf4b965a10a942371ea4066ac661af321262
Kayla S <kaylaesmith1@gmail.com>
2025-03-25 10:32:56 +0000

    Troubleshoot-map markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,25 +1,28 @@
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 1.75,
         center: {
             lat: 46.619261,
             lng: -39.134766
         }
     });
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var locations = [
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const locations = [
         // BCN //
         {lat: 41.390205, lng: 2.154007},
         // DSM //
         {lat: 41.5878, lng: -93.6192}
         
     ];
-    var marker = locations.map(function(location, i) {
+    const markers = locations.map((location, i) => {
         return new google.maps.Marker({
-            position: location, 
+            position: location,
             label: labels[i % labels.length]
         });
     });
-    var markerCluster = new MarkerClusterer(map, marker,
-    {imagePath: `https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m`});
+    const markerCluster = new MarkerClusterer({ map, markers });
 }
+
+window.initMap = initMap;
