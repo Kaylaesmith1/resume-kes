0c3014c0f56844c0f642867a7633cc2dda65b282
Kayla S <kaylaesmith1@gmail.com>
2025-03-25 17:16:23 +0000

    Troubleshoot-email contact form

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,12 @@
 function initMap() {
     // Set Teruel as the center of the map
     const barcelona = { lat: 41.390205, lng: 2.154007 };
-    const teruel = { lat: 40.344109, lng: -1.106910 };
+    // const teruel = { lat: 40.344109, lng: -1.106910 };
 
     
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 4, 
-        center: teruel,
+        center: barcelona,
     });
 
     
