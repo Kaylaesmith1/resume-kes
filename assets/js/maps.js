60a6f2e2984f9545db4dfc2330c9e5fc2a028a01
Kayla S <kaylaesmith1@gmail.com>
2025-03-25 12:21:23 +0000

    change map markers

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,34 +1,17 @@
-// import { MarkerClusterer } from "@googlemaps/markerclusterer";
-
 function initMap() {
+    // Set Barcelona as the center of the map
+    const barcelona = { lat: 41.390205, lng: 2.154007 };
+
+    // Create the map
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 1.75,
-        center: {
-            lat: 46.619261,
-            lng: -39.134766
-        }
+        zoom: 10,  // Adjust zoom level as needed
+        center: barcelona,
     });
-    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const locations = [
-        // BCN //
-        {lat: 41.390205, lng: 2.154007},
-        // DSM //
-        {lat: 41.5878, lng: -93.6192}
-        
-    ];
 
-    const markers = locations.map((location, i) => {
-        return new google.maps.marker.AdvancedMarkerElement({
-            position: location,
-            map: map,
-            label: labels[i % labels.length]
-        });
-    });    
-
-const markerCluster = new MarkerClusterer({ 
-map: map, 
-markers: markers
- });
+    // Add a marker in Barcelona
+    new google.maps.Marker({
+        position: barcelona,
+        map: map,
+        title: "Barcelona",
+    });
 }
-
-window.initMap = initMap;
