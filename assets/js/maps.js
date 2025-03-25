979b8c81b0182c8c45318c83b27b642ec9acc2f8
Kayla S <kaylaesmith1@gmail.com>
2025-03-25 17:30:36 +0000

    Troubleshoot map center

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,18 +1,25 @@
 function initMap() {
-    // Set Teruel as the center of the map
-    const barcelona = { lat: 41.390205, lng: 2.154007 };
-    // const teruel = { lat: 40.344109, lng: -1.106910 };
+    // New center to shift the map slightly left
+    const centerPosition = { lat: 40.344109, lng: -1.106910 };
 
-    
+    // Create the map
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6, 
-        center: barcelona,
+        zoom: 6,  // Adjust zoom level as needed
+        center: centerPosition,
     });
 
-    
-    new google.maps.Marker({
-        position: barcelona,
-        map: map,
-        title: "Barcelona",
+    // Marker locations
+    const locations = [
+        { lat: 41.390205, lng: 2.154007, title: "Barcelona" }  // Barcelona
+        // { lat: 41.5878, lng: -93.6192, title: "Des Moines" }
+    ];
+
+    // Add markers to the map
+    locations.forEach((location) => {
+        new google.maps.Marker({
+            position: { lat: location.lat, lng: location.lng },
+            map: map,
+            title: location.title,
+        });
     });
 }
